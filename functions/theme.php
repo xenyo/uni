066eb0b70eea95a7885107763d1f3f0410840559
Parkle Lee <parkle.lee@xenyo.com>
2022-10-21 11:32:57 +0800

    Refactor layouts

---
+++ functions/theme.php
@@ -0,0 +1,16 @@
+<?php
+
+function uni_features_theme() {
+  return [
+    'layout__section' => [
+      'template' => 'layout/layout--section/layout--section',
+      'render element' => 'content',
+      'base hook' => 'layout',
+    ],
+    'layout__section__basic' => [
+      'template' => 'layout/layout--section--basic/layout--section--basic',
+      'render element' => 'content',
+      'base hook' => 'layout__section',
+    ],
+  ];
+}
