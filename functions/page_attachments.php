32599d60a2f07ffb8b5bd7e971a770a9616b1ec7
Parkle Lee <parkle.lee@xenyo.com>
2022-10-28 16:31:53 +0800

    Rework section and wrapper

---
+++ functions/page_attachments.php
@@ -0,0 +1,6 @@
+<?php
+
+function uni_features_page_attachments(&$attachments) {
+  $attachments['#attached']['library'][] = 'uni_features/fontawesome';
+  $attachments['#attached']['library'][] = 'uni_features/wrapper';
+}
