8d1b403b44fe69b6f2fde61a5ceec0acc42bbcda
Parkle Lee <parkle.lee@xenyo.com>
2022-10-28 11:01:47 +0800

    Remove admin theme assets

--- functions/form_alter.php
+++ functions/form_alter.php
@@ -8,7 +8,6 @@ use Drupal\Core\Render\Element;
 function uni_features_form_alter(&$form, $form_state, $form_id) {
   switch ($form_id) {
     case 'layout_paragraphs_component_form':
-      $form['#attached']['library'][] = 'uni_features/layout-paragraphs-component-form';
       foreach (Element::children($form) as $key) {
         if (isset($form[$key]['widget'][0]['select'])) {
           $form[$key]['widget'][0]['select']['#empty_option'] = t('- Auto -');
@@ -47,11 +46,6 @@ function uni_features_form_alter(&$form, $form_state, $form_id) {
           ],
         ],
       ];
-      break;
-
-    case 'node_page_form':
-    case 'node_page_edit_form':
-      $form['#attached']['library'][] = 'uni_features/node-page-form';
       break;
   }
 }
