6b6ed02e549bd18b13cd046fdd25a7821d2e2cf7
Parkle Lee <parkle.lee@xenyo.com>
2022-11-29 19:05:09 +0800

    Refactor section layouts

---
+++ functions/preprocess_layout.php
@@ -0,0 +1,27 @@
+<?php
+
+use Drupal\Core\Template\Attribute;
+
+function uni_preprocess_layout(&$variables) {
+  if (!isset($variables['settings']['layout_paragraphs_section'])) {
+    return;
+  }
+
+  $paragraph = $variables['settings']['layout_paragraphs_section']->getEntity();
+
+  // Add wrapper attributes
+  $variables['wrapper_attributes'] = new Attribute();
+  $variables['wrapper_attributes']->addClass('uni-wrapper');
+  if ($paragraph->hasField('uni_section_width')) {
+    $width = $paragraph->get('uni_section_width')->getString();
+    if ($width !== 'standard') {
+      $variables['wrapper_attributes']->addClass('uni-wrapper--' . $width);
+    }
+  }
+
+  // Add region attributes
+  foreach ($variables['region_attributes'] as $region => $attributes) {
+    $attributes->addClass('region');
+    $attributes->addClass('region--' . $region);
+  }
+}
