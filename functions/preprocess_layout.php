4d70100dd049b789a1925dd7a207fa64f6d7fd69
Parkle Lee <parkle.lee@xenyo.com>
2022-12-28 11:41:08 +0800

    Fix wrapper classes

--- functions/preprocess_layout.php
+++ functions/preprocess_layout.php
@@ -11,32 +11,9 @@ function uni_preprocess_layout(&$variables) {
 
   // Add wrapper attributes
   $variables['wrapper_attributes'] = new Attribute([ 'class' => 'uni-wrapper' ]);
-  if ($paragraph->hasField('uni_section_wrapper_classes')) {
-    $terms = $paragraph->uni_section_wrapper_classes->referencedEntities();
-    foreach ($terms as $term) {
-      $variables['wrapper_attributes']->addClass($term->label());
-    }
+  if ($paragraph->hasField('uni_section_horizontal')) {
+    $class = $paragraph->get('uni_section_horizontal');
+    $variables['wrapper_attributes']->addClass($class->getString());
   }
 
-  // Add layout attributes
-  if ($paragraph->hasField('uni_section_layout_classes')) {
-    $terms = $paragraph->uni_section_layout_classes->referencedEntities();
-    foreach ($terms as $term) {
-      $variables['attributes']['class'][] = $term->label();
-    }
-  }
-
-  // Add region attributes
-  foreach ($variables['region_attributes'] as $region => $attributes) {
-    $attributes->addClass('region');
-    $attributes->addClass('region--' . $region);
-  }
-  if ($paragraph->hasField('uni_section_region_classes')) {
-    $terms = $paragraph->uni_section_region_classes->referencedEntities();
-    foreach ($terms as $term) {
-      foreach ($variables['region_attributes'] as $region => $attributes) {
-        $attributes->addClass($term->label());
-      }
-    }
-  }
 }
