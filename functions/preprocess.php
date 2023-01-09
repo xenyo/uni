4d3f4781714e8421b57cf067ca8afbe019ba7d1f
Parkle Lee <parkle.lee@xenyo.com>
2023-01-09 10:36:04 +0800

    Revert "Remove pattern attributes"
    This reverts commit c5faad5fc00c7dc592717adeaa37b24ab7c085a9.

--- functions/preprocess.php
+++ functions/preprocess.php
@@ -7,4 +7,20 @@ function uni_preprocess(&$variables, $hook, $info) {
   $variables['base_path'] = base_path();
   $variables['path'] = base_path() . $info['path'];
   $variables['theme_path'] = base_path() . $info['theme path'];
+
+  if (substr($hook, 0, 8) === 'pattern_') {
+    $id = Html::cleanCssIdentifier(substr($hook, 8));
+    $variables['attributes'] = AttributeHelper::mergeCollections(
+      $variables['attributes'] ?? [],
+      [ 'class' => $id ],
+    );
+
+    $variant = Html::cleanCssIdentifier($variables['variant']);
+    if (!empty($variant)) {
+      $variables['attributes'] = AttributeHelper::mergeCollections(
+        $variables['attributes'] ?? [],
+        [ 'class' => "$id--$variant" ],
+      );
+    }
+  }
 }
